75ac3cb72b4b52d89735cc794a425d28a9ec24b0
Brent Magnusson <bmagnusson@ascendanalytics.com>
2022-02-04 11:43:24 -0700

    Lesson 3 stub

---
+++ 03_classes.py
@@ -0,0 +1,17 @@
+"""
+Go through https://www.w3schools.com/python/python_classes.asp, writing down code here and using
+PyCharm to run the debugger and inspect the class as you build it
+"""
+
+"""
+Problem 1:
+In the w3 tutorial, you built a class called Person. Make a new class called Robot that does the 
+same thing as Person, but includes an attribute called "processing_unit" that is printed after
+"Hello my name is <name>". 
+
+So, if the Robot has "name=zuckerberg" and "processing_unit=hpu_v3", when `myfunc` is called, the 
+printed output should be:
+
+Hello my name is zuckerberg
+My processing unit is hpu_v3
+"""
